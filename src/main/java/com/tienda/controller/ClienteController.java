c1bf830a32d18379e7d385d145a7083c2bf030fe
Michael <Michael@Minerva>
2022-04-28 18:10:24 -0600

    last commit

--- src/main/java/com/tienda/controller/ClienteController.java
+++ src/main/java/com/tienda/controller/ClienteController.java
@@ -1,15 +1,22 @@
-
 package com.tienda.controller;
 
 import com.tienda.domain.Cliente;
+import com.tienda.service.ClienteReportService;
 import com.tienda.service.ClienteService;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  *
  * @author Michael
@@ -17,11 +24,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 
 public class ClienteController {
-    
+
     @Autowired
     private ClienteService clienteService;
-    
-    
+
+    @Autowired
+    private ClienteReportService clienteReportService;
 //    @GetMapping("/cliente/listado")
 //    public String inicio(Model model) {
 //        var clientes = clienteService.getClientes();
@@ -29,41 +37,62 @@ public class ClienteController {
 //        
 //        return "/cliente/listado";
 //    }
+
     @GetMapping("/cliente/listado")
     public String inicio(Model model) {
-        var clientes=clienteService.getClientes();
-        
-        var limiteTotal=0;
-        for (var c: clientes) {
-            limiteTotal+=c.getCredito().getLimite();
+        var clientes = clienteService.getClientes();
+
+        var limiteTotal = 0;
+        for (var c : clientes) {
+            limiteTotal += c.getCredito().getLimite();
         }
-        model.addAttribute("limiteTotal",limiteTotal);
-        model.addAttribute("totalClientes",clientes.size());
-        
-        model.addAttribute("clientes",clientes);
+        model.addAttribute("limiteTotal", limiteTotal);
+        model.addAttribute("totalClientes", clientes.size());
+
+        model.addAttribute("clientes", clientes);
         return "/cliente/listado";
     }
-    
+
     @GetMapping("/cliente/nuevo")
-    public String nuevoCliente(Cliente cliente){
+    public String nuevoCliente(Cliente cliente) {
         return "/cliente/modifica";
     }
-    
+
     @PostMapping("/cliente/guardar")
-    public String guardarCliente(Cliente cliente){
+    public String guardarCliente(Cliente cliente) {
         clienteService.save(cliente);
         return "redirect:/cliente/listado";
     }
+
     @GetMapping("/cliente/modificar/{idCliente}")
-    public String modificarCliente(Cliente cliente, Model model){
+    public String modificarCliente(Cliente cliente, Model model) {
         var respuesta = clienteService.getCliente(cliente);
         model.addAttribute("cliente", respuesta);
         return "/cliente/modifica";
     }
-    
+
     @GetMapping("/cliente/eliminar/{idCliente}")
-    public String eliminarCliente(Cliente cliente){
+    public String eliminarCliente(Cliente cliente) {
         clienteService.delete(cliente);
         return "redirect:/cliente/listado";
     }
+
+    @GetMapping(value = "/cliente/ReporteClientes", produces = MediaType.APPLICATION_PDF_VALUE)
+    public @ResponseBody
+    byte[] getFile() throws IOException {
+        try {
+            FileInputStream fis = new FileInputStream(new File(clienteReportService.generateReport()));
+            byte[] targetArray = new byte[fis.available()];
+            fis.read(targetArray);
+            return targetArray;
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
